57d18f40449da1499efe79ff82f2258da999251f
Renan William Alves de Paula <renan@4success.com.br>
2021-02-12 17:22:02 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Template -  Delta Batch Integration Flow
+## NodejS  / Typescript
+
+This is a basic template for automations of type "Delta Batch". Use it as a start point of your automation.
+
+The "Delta" integration flow basically extract data from one or more sources, process all items at once and save this execution result as a delta image.
+In the next execution, it will consider only changes between the last extraction.
+
+It is designed for maximum flexibility but have a behavior of 'all or nothing'. This flow save a lot of processing time using delta images but use more memory.
+
+### Default configuration:
+* Environment: nodejs12.x
+* Memory: 512mb
+* Timeout: 60s
+
+You can personalize this settings in tunnelhub.yml file
+
+### Intructions:
+* You can install all dependencies with `npm install` or `yarn`.
+* Your main logic is in `src/index.ts` file. 
+* You can check our example test in `__tests__` folder. Our tests are writted using [Jest](https://www.npmjs.com/package/jest). 
+* To run your tests, just run `npm run test`
+* To deploy your automation, it's necessary zip all your project in a zip file. Use `npm run build` to transpile all your code and libraries using babel and save it in `dist` folder.
+
+Your preferred deploy command:
+* `npm run build && th deploy-automation --env ENVNAME --message "Deploy message"`
---
+++ __tests__/main.test.ts
@@ -0,0 +1,76 @@
+import { mocked } from 'ts-jest/utils';
+import * as main from '../src';
+import { DeltaIntegrationFlow } from '@4success/tunnelhub-sdk/src/classes/flows/deltaIntegrationFlow';
+import AutomationLog from '@4success/tunnelhub-sdk/src/classes/logs/automationLog';
+import AutomationDelta from '@4success/tunnelhub-sdk/src/classes/logs/automationDelta';
+import got from 'got';
+import * as mysql from 'mysql';
+
+jest.mock('got');
+jest.mock('mysql');
+
+const mockedGot = mocked(got, true);
+const mockedMysql = mocked(mysql, true);
+
+beforeAll(() => {
+  /**
+   * The code bellow is ** mandatory ** to avoid TunnelHub SDK make external calls trying persist logs
+   * You can make this mock using the same code with any IntegrationFlow at @4success/tunnelhub-sdk/classes/flows
+   */
+  const persistLogsFunc = jest.spyOn(AutomationLog.prototype as any, 'save');
+  persistLogsFunc.mockImplementation(() => {
+  });
+
+  const saveDelta = jest.spyOn(AutomationDelta.prototype as any, 'save');
+  saveDelta.mockImplementation(() => {
+  });
+
+  const updateExecutionStatisticsFunc = jest.spyOn(DeltaIntegrationFlow.prototype as any, 'updateExecutionStatistics');
+  updateExecutionStatisticsFunc.mockImplementation(() => {
+  });
+});
+
+type CallbackFunction = (error: any | null, results: any[], fields: string[]) => void;
+
+test('testOnyInserts', async () => {
+  /***
+   * Mocking mysql class and got
+   */
+  //@ts-ignore - is not necessary return all methods
+  mockedMysql.createConnection.mockImplementation(() => {
+    return {
+      query: (query: string, values?: string[] | CallbackFunction, callback?: CallbackFunction) => {
+        switch (query) {
+          case 'SELECT * FROM covidCases':
+            if (typeof callback === 'function') {
+              callback(null, [], []);
+            } else if (typeof values === 'function') {
+              values(null, [], []);
+            }
+            break;
+          default:
+            if (typeof callback === 'function') {
+              callback(null, [], []);
+            } else if (typeof values === 'function') {
+              values(null, [], []);
+            }
+        }
+      },
+      end: () => {
+
+      },
+    };
+  });
+
+  //@ts-ignore - is not necessary return all methods
+  mockedGot.mockReturnValue({ body: JSON.stringify(require('./data/covidCases.json')) });
+  /**
+   * Calling my function
+   */
+  const response = await main.handler({});
+
+  expect(response.statusCode).toEqual(200);
+  expect(typeof response.body).toBe('string');
+
+  expect(response.body).toEqual('Automation executed with no errors!');
+});
